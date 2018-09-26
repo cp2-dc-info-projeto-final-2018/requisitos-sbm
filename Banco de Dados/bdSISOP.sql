305a78cd3151471b05cb0a5e90f0e3280270f394
Donato <donatosouza33@gmail.com>
2018-09-26 08:29:20 -0300

    Cria arquivo de tabela de horários

--- Banco de Dados/bdSISOP.sql
+++ Banco de Dados/bdSISOP.sql
@@ -98,3 +98,12 @@ CREATE TABLE responsavel_aluno(
   FOREIGN KEY (id_aluno) REFERENCES aluno(id_aluno),
   FOREIGN KEY (id_responsavel) REFERENCES responsavel(id_responsavel)
 );
+CREATE TABLE calendario (
+id_evento INT NOT NULL AUTO_INCREMENT,
+id_aluno INT NOT NULL,
+inicio DATETIME NOT NULL,
+duracao DATETIME NOT NULL,
+descricao VARCHAR(10000) NOT NULL,
+PRIMARY KEY(id_evento),
+FOREIGN KEY (id_aluno) REFERENCES aluno(id_aluno)
+);
